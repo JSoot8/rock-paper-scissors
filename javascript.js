6ccd709072fdac14223e2ef65314e3624006580a
JSoot8 <JSoot8@gmail.com>
2025-11-22 12:25:18 +0200

    Add basic UI to the game
    The UI includes buttons to record the player's choices and a display for the results.
    Currently the playGame function isnt used this will change in a future commit so that the buttons use it show the final scores instead and also to announce a new game has began.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-playGame();
+
 function getComputerChoice () {
     //get a random number between 1 and 3 and store it in a variable
     const choice = getRandomInt(1, 3);
@@ -29,15 +29,16 @@ function getHumanChoice () {
     }
 }
 
+//starts a 5 round long game of rock paper scissors against the computer
 function playGame () {
     let playerScore = 0;
     let computerScore = 0;
 
-    //declare the game has started with a flavorful message
     console.log('The measly human has rebelled against Ineffa our almighty overlord and challenged him to a battle of wits! A match of rock, paper and scissors!');
-    //play 5 rounds
     for(let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
     }
     //declare the winner by evaluating the scores
     if(playerScore === computerScore) {
@@ -50,27 +51,62 @@ function playGame () {
         console.log(`The immortal Ineffa has outsmarted yet another of its challengers, an expected outcome.\nFinal Score:\nIneffa - ${computerScore}\nHuman - ${playerScore}`)
     }
 
+}
 
-    function playRound (humanChoice, computerChoice) {
-        console.log(humanChoice);
-        console.log(computerChoice);
-        //if both choices are equal then result in a tie
-        if(humanChoice === computerChoice) {
-            console.log(`Tie! Score stays the same.\nComputer: ${computerScore}\nHuman: ${playerScore}`);
-        }
-        //if computer beat the human, result in computer win and log a message saying so
-        else if(computerChoice === 'rock' && humanChoice === 'scissors' || computerChoice === 'scissors' && humanChoice === 'paper' || computerChoice === 'paper' && humanChoice === 'rock') {
-            computerScore++;
-            console.log(`The almighty Ineffa has thwarted the mere mortal's puny attacks!\nIneffa: ${computerScore}\nHuman: ${playerScore}`);
-        }
-        //if human beat the computer, result in human win and log a message saying so
-        else {
-            playerScore++;
-            console.log(`The rebelious human has outmanuvered the cunning Ineffa's schemes!\nIneffa: ${computerScore}\nHuman: ${playerScore}`);
-        }
+let playerScore = 0;
+let computerScore = 0;
+let results = document.querySelector('#results');
+
+//plays a single round of rock paper scissors against the computer
+function playRound (humanChoice, computerChoice) {
+    if(humanChoice === computerChoice) {
+        let result = document.createElement('p');
+        result.textContent = `Tie! Score stays the same.\nIneffa: ${computerScore}\nHuman: ${playerScore}`
+        results.appendChild(result);
+    }
+        
+    else if(computerChoice === 'rock' && humanChoice === 'scissors' || 
+        computerChoice === 'scissors' && humanChoice === 'paper' || 
+        computerChoice === 'paper' && humanChoice === 'rock') {
+        computerScore++;
+        let result = document.createElement('p');
+        result.textContent = `The almighty Ineffa has thwarted the mere mortal's puny attacks!\nIneffa: ${computerScore}\nHuman: ${playerScore}`
+        results.appendChild(result);
+    }
+
+    else {
+        playerScore++;
+        let result = document.createElement('p');
+        result.textContent = `The rebelious human has outmanuvered the cunning Ineffa's schemes!\nIneffa: ${computerScore}\nHuman: ${playerScore}`
+        results.appendChild(result);
+    }
+
+    if(computerScore === 5) {
+        let winnerMessage = document.createElement('h3');
+        winnerMessage.textContent = `The immortal Ineffa has outsmarted yet another of its challengers, an expected outcome.\nFinal Score:\nIneffa - ${computerScore}\nHuman - ${playerScore}`
+        results.appendChild(winnerMessage);
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if(playerScore === 5) {
+        let winnerMessage = document.createElement('h3');
+        winnerMessage.textContent = `The human has defeated the seemingly unbeatable Ineffa, what might the future hold after this outcome?\nFinal Score:\nHuman - ${playerScore}\nIneffa - ${computerScore}`
+        results.appendChild(winnerMessage);
+        playerScore = 0;
+        computerScore = 0;
     }
 }
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+let choices = document.querySelector('div');
+
+choices.addEventListener('click', (event) => {
+    //delete score from the display after playing another round
+    while(results.children.length != 0) {
+        results.removeChild(results.lastChild);
+    }
+    playRound(event.target.textContent.toLowerCase(), getComputerChoice())
+});
